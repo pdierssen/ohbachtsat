ba7d40a85fc2fc03d33a7d2214ee468a1596de2e
jl <chico.2014.jb@gmail.com>
2025-10-13 14:53:25 +0200

    feat: added temperature_humidity and light sensor

--- lib/Gyro/Gyro.h
+++ lib/Gyro/Gyro.h
@@ -7,7 +7,7 @@ int readMeasurement();
 void storeMeasurements();
 void visualizeMeasurements(String heading, int16_t x, int16_t y, int16_t z);
 void gyro_setup();
-void gyro_accellerometer();
+void gyro_accelerometer();
 void gyro_gyrometer();
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,24 @@
 #include <Arduino.h>
-
 #include "Blink.h"
 #include "OLED.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "Gyro.h"
-
-// put function declarations here:
-int myFunction(int, int);
+#include "Temperature_humidity.h"
+#include "Brightness.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
   blink_setup();
   gyro_setup();
+  dht11Setup();
+  setupBH1750();
   OLED::instance();
 }
 
 void loop() {
-  //Startup: Logo pof some sort, Constants: duration
-  //Gyrometer x, y, z Constants: resfresh rate, duration
-  //Accelreometer x, y, z Constants: resfresh rate, duration
-  //Lightsensor value Constants: resfresh rate, duration
-  //Humidity sensor value Constants: resfresh rate, duration
-  //Temperature x, y, z Constants: resfresh rate, duration
-
-  blink_loop();
-  
-  OLED::instance().getDisplay().println("Hallo");
-  OLED::instance().getDisplay().display();
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  gyro_gyrometer();
+  gyro_accelerometer();
+  temperature_humidity();
+  bh1750();
 }
